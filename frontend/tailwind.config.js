a42fc361ca4e04fddce94b45db4e67fa871426c0
yashdesai023 <131481202+yashdesai023@users.noreply.github.com>
2025-05-13 01:03:32 +0530

    Backend + frontend base structure

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        primary: '#7C3AED',
+        dark: '#1E293B',
+      },
+    },
+  },
+  plugins: [],
+} 
